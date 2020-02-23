309e8adba19444412b639ca56812465b66a44453
Katherine Ruiz Sarria <kruiz@swapps.co>
2020-02-22 21:19:12 -0500

    remove item when item expired

--- LRUCache.py
+++ LRUCache.py
@@ -1,4 +1,6 @@
 import collections
+import threading
+
 class LRUCache(object):
     items = {}
     capacity = 0
@@ -10,12 +12,19 @@ class LRUCache(object):
         self.capacity = capacity
         self.my_list = collections.deque([])
         self.items = {}
-        #print(self.my_list)
+
+    def remove_expired_item(self, key):
+        if key in self.items.keys():
+            del self.items[key]
+            self.my_list.remove(key)
+
 
     def get(self, key):
         """
         :type key: int
         :rtype: int
+        if the key in dict exists then return value and remove the item from
+        the current deque position and push it again to the top left 
         """
         if key in self.items:
             self.my_list.remove(key)
@@ -25,11 +34,15 @@ class LRUCache(object):
         return -1
         
 
-    def put(self, key, value):
+    def put(self, key, value, time_limit=0):
         """
         :type key: int
         :type value: int
         :rtype: None
+        """
+        """
+        If the length dict items are less of capacity, then add/update the item 
+        to the dict and append left to the deque.
         """
         if len(self.items) <= self.capacity:
             if key not in self.items.keys():
@@ -39,13 +52,21 @@ class LRUCache(object):
                 self.my_list.appendleft(key)
             self.items[key] = value
                 
-                
+        """
+        If the dict items are greater than capacity then I pop the last item
+        and delete the item from dict
+        """        
         if len(self.items) == self.capacity+1 and self.my_list:
             key = self.my_list.pop()
             if key in self.items:
                 del self.items[key]
+        """
+        If the item expired, then trigger the function to remove it in the time limit
+        """
+        if time_limit > 0:
+            threading.Timer(time_limit, self.remove_expired_item, [key]).start()
     
-    def getAll(self):
+    def get_all(self):
         return self.items, self.my_list
         
         
