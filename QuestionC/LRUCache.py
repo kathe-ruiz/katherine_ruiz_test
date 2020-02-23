788859844b291ace91545a075a717e7411c49fea
Katherine Ruiz Sarria <kruiz@swapps.co>
2020-02-23 18:21:26 -0500

    add logging system

--- QuestionC/LRUCache.py
+++ QuestionC/LRUCache.py
@@ -1,11 +1,17 @@
 import collections
 import threading
+import logging
+import json
+from ast import literal_eval
 
 
 class LRUCache(object):
     items = {}
     capacity = 0
     my_list = collections.deque([])
+    logging = None
+    CACHE_LOG = "cache.log"
+    KEY_WORD = "Logging"
 
     def __init__(self, capacity):
         """
@@ -14,11 +20,44 @@ class LRUCache(object):
         self.capacity = capacity
         self.my_list = collections.deque([])
         self.items = {}
+        logging.basicConfig(filename=self.CACHE_LOG, level=logging.DEBUG)
+        # retreive data when server is up
+        self.get_logging()
 
     def remove_expired_item(self, key):
         if key in self.items.keys():
             del self.items[key]
             self.my_list.remove(key)
+
+    def clear_logging(self):
+        open(self.CACHE_LOG, "w").close()
+
+    def save_log(self):
+        logging.info("Logging{}-{}".format(str(self.items), str(list(self.my_list))))
+
+    def get_logging(self):
+        """
+            get last record from log and clear the log
+        """
+        try:
+            with open(self.CACHE_LOG, "r") as f:
+                lines = f.read().splitlines()
+                line = lines[-1]
+                if self.KEY_WORD in line:
+                    line = line.strip()
+                    index = line.index(self.KEY_WORD)
+                    values = (
+                        line[index + len(self.KEY_WORD) :].replace("'", '"').split("-")
+                    )
+                    python_dict = literal_eval(values[0])
+                    python_list = values[1].strip("][").split(", ")
+                    python_list = [int(x) for x in python_list]
+                    self.items = python_dict
+                    self.my_list = collections.deque(python_list)
+                    self.clear_logging()
+                    self.save_log()
+        except Exception as e:
+            print("Error Get Logging %s", e)
 
     def get(self, key):
         """
@@ -30,8 +69,8 @@ class LRUCache(object):
         if key in self.items:
             self.my_list.remove(key)
             self.my_list.appendleft(key)
+            self.save_log()
             return self.items[key]
-
         return -1
 
     def put(self, key, value, time_limit=0):
@@ -63,6 +102,7 @@ class LRUCache(object):
         """
         If the item expired, then trigger the function to remove it in the time limit
         """
+        self.save_log()
         if time_limit > 0:
             threading.Timer(time_limit, self.remove_expired_item, [key]).start()
 
